b719c1ea3ec72b03d40a0de3e705b4b5206ed860
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-09-19 17:20:33 -0500

    feat(General): added main

---
+++ cmake-build-debug/CMakeFiles/cs2100_trees_LuisUTEC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs2100_trees_LuisUTEC.dir/main.cpp.obj"
+  "cs2100_trees_LuisUTEC.pdb"
+  "cs2100_trees_LuisUTEC.exe"
+  "cs2100_trees_LuisUTEC.exe.manifest"
+  "libcs2100_trees_LuisUTEC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs2100_trees_LuisUTEC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cs2100_trees_LuisUTEC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cs2100_trees_LuisUTEC.
+# This may be replaced when dependencies are built.
